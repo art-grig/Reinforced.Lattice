c94fd4dda6540138f9dcc898229e3ea11bd25311
Pavel Novikov <pavel.b.novikov@gmail.com>
2016-04-29 22:19:55 +0600

    template switcher

---
+++ PowerTables.Mvc/Scripts/templateswitch.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+    $('#tplSwitch').on('change', function() {
+        var val = $('#tplSwitch').val();
+        var urlPart = window.location.href;
+        if (urlPart.indexOf('?') > -1) {
+            urlPart = urlPart.substring(0, urlPart.indexOf('?'));
+        }
+        urlPart += "?tpl=" + val;
+        window.location.href = urlPart;
+    });
+});
