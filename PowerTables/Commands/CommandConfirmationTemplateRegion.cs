7d9af7e3d5d71a5c51128c0e1e67b43a1f8e69ca
Pavel Novikov <pavel.b.novikov@gmail.com>
2016-11-14 03:42:56 +0200

    command description

---
+++ PowerTables/Commands/CommandConfirmationTemplateRegion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using PowerTables.Plugins.Limit;
+using PowerTables.Plugins.Toolbar;
+using PowerTables.Templating;
+using PowerTables.Templating.Handlebars;
+
+namespace PowerTables.Commands
+{
+    public class CommandConfirmationTemplateRegion<TRow,TConfirmation> : PluginTemplateRegion
+        , IModelProvider<IConfirmationViewModel<TRow, TConfirmation>>
+    {
+        public CommandConfirmationTemplateRegion(IViewPlugins page, string id) : base(page, id)
+        {
+        }
+
+        public string ExistingModel { get; private set; }
+    }
+
+    
+    /// <summary>
+    /// Command confirmation viewmodel
+    /// </summary>
+    public interface IConfirmationViewModel<TRow, TConfirmation>
+    {
+        IHbArray<TRow> Selection { get; set; }
+
+        TRow Subject { get; set; }
+    }
+
+    public static class CommandConfirmationTemplateExtensions
+    {
+        /// <summary>
+        /// Template region for limit plugin
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static CommandConfirmationTemplateRegion<TRow, TConfirmation> Confirmation<TRow, TConfirmation>(this IViewPlugins t, string templateId)
+        {
+            return new CommandConfirmationTemplateRegion<TRow, TConfirmation>(t, templateId);
+        }
+        /// <summary>
+        /// Template region for limit plugin
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static CommandConfirmationTemplateRegion<object, object> Confirmation(this IViewPlugins t, string templateId)
+        {
+            return new CommandConfirmationTemplateRegion<object, object>(t, templateId);
+        }
+
+        public static MvcHtmlString BindDismiss<TRow, TForm>(this  CommandConfirmationTemplateRegion<TRow, TForm> p, string eventId)
+        {
+            return p.BindEvent("dismiss", eventId);
+        }
+
+        public static MvcHtmlString BindConfirm<TRow, TForm>(this  CommandConfirmationTemplateRegion<TRow, TForm> p, string eventId)
+        {
+            return p.BindEvent("confirm", eventId);
+        }
+    }
+}
---
+++ PowerTables/Commands/CommandDescription.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using PowerTables.Editing;
+using PowerTables.Plugins.Formwatch;
+
+namespace PowerTables.Commands
+{
+    public class CommandDescription
+    {
+        public string Name { get; set; }
+
+        public JRaw ClientFunction { get; set; }
+        
+        public JRaw CanExecute { get; set; }
+
+        public CommandType Type { get; set; }
+    }
+
+    public class ConfirmationConfiguration
+    {
+        public string TemplateId { get; set; }
+
+        public string TargetSelector { get; set; }
+
+        /// <summary>
+        /// Gets or sets confirmation form fields configuration
+        /// </summary>
+        public List<FormwatchFieldData> Formwatch { get; set; }
+
+        public CommandAutoformConfiguration Autoform { get; set; }
+
+        public DetailLoadingConfiguration Details { get; set; }
+
+        public string ContentLoadingUrl { get; set; }
+
+        public string ContentLoadingCommand { get; set; }
+    }
+
+    public class CommandAutoformConfiguration
+    {
+        public EditFormUiConfigBase Autoform { get; set; }
+
+        public bool DisableWhenContentLoading { get; set; }
+
+        public bool DisableWhileDetailsLoading { get; set; }
+    }
+
+    public class DetailLoadingConfiguration
+    {
+        public string CommandName { get; set; }
+
+        public bool LoadImmediately { get; set; }
+
+        public JRaw ValidateToLoad { get; set; }
+    }
+
+    public enum CommandType
+    {
+        Client,
+        Server
+    }
+}
