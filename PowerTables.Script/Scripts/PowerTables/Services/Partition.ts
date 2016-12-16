84f6e3c7d9c685a45de27b1df52d8617d44e94f8
Pavel Novikov <pavel.b.novikov@gmail.com>
2016-12-16 11:41:37 +0700

    data partition begin

---
+++ PowerTables.Script/Scripts/PowerTables/Services/Partition.ts
@@ -0,0 +1,17 @@
+﻿module PowerTables.Services {
+    export class PartitionService {
+        public Skip: number;
+        public Take: number;
+
+        public IsAllDataRetrieved: boolean;
+        public IsTotalCountKnown: boolean;
+
+        public setSkip(skip: number) {
+            
+        }
+
+        public setTake(take?: number) {
+            
+        }
+    }
+}
