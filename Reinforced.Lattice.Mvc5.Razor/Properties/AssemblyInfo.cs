17beca95944e08c6d79b345455b99761fb51c1d4
Pavel Novikov <pavel.b.novikov@gmail.com>
2017-02-13 18:48:39 +0700

    version bump

--- Reinforced.Lattice.Mvc5.Razor/Properties/AssemblyInfo.cs
+++ Reinforced.Lattice.Mvc5.Razor/Properties/AssemblyInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("1.0.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.1")]
 
 
